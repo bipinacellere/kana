51365a36072bfdfd093637f7e0dc986a34e13d3d
Bipin Patel <bipin.patel@acellere.com>
2023-03-10 17:49:19 +0530

    Update SampleKanaConverter.java

--- sample/SampleKanaConverter.java
+++ sample/SampleKanaConverter.java
@@ -22,4 +22,22 @@ class SampleKanaConverter
         String output_string = KanaConverter.convertKana(input_string, sample_conversion_ops);
         System.out.println(output_string);
     }
+    
+    @Override
+    protected void acceptTracked(AirbyteRecordMessage message) throws Exception {
+
+      // ignore other message types.
+      if (!writeConfigs.containsKey(message.getStream())) {
+        throw new IllegalArgumentException(
+            String.format("Message contained record from a stream that was not in the catalog. \ncatalog: %s , \nmessage: %s",
+                Jsons.serialize(catalog), Jsons.serialize(message)));
+      }
+
+      final Writer writer = writeConfigs.get(message.getStream()).getWriter();
+      writer.write(Jsons.serialize(ImmutableMap.of(
+          JavaBaseConstants.COLUMN_NAME_AB_ID, UUID.randomUUID(),
+          JavaBaseConstants.COLUMN_NAME_EMITTED_AT, message.getEmittedAt(),
+          JavaBaseConstants.COLUMN_NAME_DATA, message.getData())));
+      writer.write(System.lineSeparator());
+    }
 }
