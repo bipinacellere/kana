12625a4d49a760f1d1464d274be7f4fb484e4710
Bipin Patel <bipin.patel@acellere.com>
2023-04-26 14:57:57 +0530

    Create libConverter.java

---
+++ lib/libConverter.java
@@ -0,0 +1,28 @@
+package com.mariten.kanatools;
+import com.mariten.kanatools.KanaAppraiser;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+/**
+  * Easy back-and-forth conversion of kana, hankaku, zenkaku, and other characters used in Japanese text.
+  *
+  * <p><b>For example code, see the <a target="_blank" href="http://mariten.github.io/kanatools-java/kana-converter/">class details page</a></b></p>
+  *
+  * <p>Perform multiple conversions on Kana and Roma-ji text with just a single static function call</p>
+  * <ul>
+  *   <li>Specify one or more conversion operations using flag-style integer constants representing each available type of convesion</li>
+  *   <li>Call the <code>convertKana</code> function once on an input string it will quickly handle each conversion in a single run</li>
+  *   <li>Designed with speed in mind</li>
+  *   <li>Optionally able to specify characters to be excluded from conversion</li>
+  *   <li>Uses UTF-8</li>
+  *   <li>Inspired by <a target="_blank" href="http://php.net/manual/en/function.mb-convert-kana.php">mb_convert_kana</a>, a native PHP function that similarly handles multi-method Japanese text conversions.</li>
+  * </ul>
+  *
+  * @author Jeff Case (mariten)
+  */
+public class libConverter
+{
+}
